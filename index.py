d10243213615dc9bcf408b8df8338f0a248134e2
hanzichi <hanzichi@outlook.com>
2018-08-02 21:24:58 +0800

    init

---
+++ index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: UTF-8 -*-
+
+from workflow import Workflow
+from pypinyin import pinyin
+import sys
+reload(sys) 
+# Python 2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入
+sys.setdefaultencoding('utf-8')
+
+
+def main(wf):
+    query = wf.args[0]
+
+    str = ' '.join([item[0] for item in pinyin(query)])
+
+    wf.add_item(title=str)
+    wf.send_feedback()
+    
+
+if __name__ == '__main__':
+    
+    wf = Workflow()
+
+    logger = wf.logger
+
+    sys.exit(wf.run(main))
